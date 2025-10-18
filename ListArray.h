b56c32423f12523becec89665a8fc053c1eb051b
RNOGMON <rnogmon@alumno.upv.es@ldsic-vdi10.upvnet.upv.es>
2025-10-18 18:35:20 +0200

    Añadida la implementacion de la clase ListArray

---
+++ ListArray.h
@@ -0,0 +1,144 @@
+#include <iostream>
+#include "List.h"
+#include <ostream>
+
+using namespace std;
+
+template <typename T>
+class ListArray : public List<T> {
+	private:
+	        T* arr;
+       		int max;
+ 		int n;
+		static const int MINSIZE = 2;
+		void resize(int new_size){
+			if(new_size < MINSIZE){
+				new_size = MINSIZE;
+			}
+			T* new_arr = new T[new_size];
+			for(int i=0; i<new_size; i++){
+				new_arr[i]= nullptr;
+			}
+			for(int i=0; i<n; i++){
+				new_arr[i] = arr[i];
+			}
+			delete[] arr;
+
+			arr = new_arr;
+			max = new_size;
+		}
+	public:
+		void insert(int pos, T e){
+			if(pos<0 || pos>max-1){
+				throw out_of_range("Posicion fuera del rango");
+			}
+			if(n == max){
+				resize(max+1);
+			}
+			if(arr[pos] != nullptr){
+				for(int i=max-1; i>pos; i--){
+					arr[i] = arr[i-1];
+				}
+			}
+			arr[pos] = e;
+			n++;
+		}
+		
+		void append(T e){
+			if(n==max){
+				resize(max+1);
+				arr[max-1] = e;
+				n++;
+			}
+			else{
+				arr[n] = e;
+				n++;
+			}
+		}
+
+		void prepend(T e){
+			if(n==max){
+				resize(max+1);
+			}
+			if(arr[0] == nullptr){
+				arr[0] = e;
+				n++;
+			}
+			else{
+				for(int i=max-1; i>0; i--){
+					arr[i] = arr[i-1];
+				}
+				arr[0] = e;
+				n++;
+			}
+		}
+
+		T remove(int pos){
+			if(pos<0 || pos>max-1){
+				throw out_of_range("Posicion fuera de rango");
+			}
+			T aux = arr[pos];
+			arr[pos] = nullptr;
+			return aux;
+		}
+
+		T get(int pos){
+			if(pos<0 || pos>max-1){
+				throw out_of_range("Posicion fuera de rango");
+			}
+			return arr[pos];
+		}
+
+		int search(T e){
+			for(int i=0; i<max; i++){
+				if(arr[i] == e){
+					return i;
+				}
+				
+			}
+			return -1;
+		}
+
+		bool empty(){
+			if(n==0){
+				return true;
+			}else{
+				return false;
+			}
+		}
+
+		int size(){
+			return n;
+		}
+
+		ListArray(){
+			arr = new T[MINSIZE];
+			max = MINSIZE;
+			n = 0;
+		}
+
+		~ListArray(){
+			delete[] arr;
+		}
+
+		T operator[](int pos){
+			if(pos<0 || pos>max-1){
+				throw out_of_range("Posicion fuera de rango");
+			}
+			else{
+				return arr[pos];
+			}
+		}		
+		
+		friend ostream&operator<<(ostream &out, const ListArray<T> &list){
+			out << "[";
+			for(int i = 0; i<list.size(); i++){
+				out << list[i];
+				if(i<list.size() -1){
+					out << ", ";
+				}
+			}
+			out << "]";
+			return out;
+		}
+};
